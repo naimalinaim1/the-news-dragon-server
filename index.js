a78679d92ed12a83c5ebe7c88f01255cf78f00e2
Naim Ali <naimalinaim1@gmail.com>
2023-04-29 19:34:46 +0600

    vercel config added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("Dragon is running");
@@ -13,6 +14,26 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const getNews = news.find((newsId) => newsId._id === id);
+  res.send(getNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
 });
 
 app.listen(PORT, () => {
